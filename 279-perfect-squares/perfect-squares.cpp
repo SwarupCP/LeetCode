7ee2b0e3d86f78aa36354e3b168b20c215a91c01
Swarup Dwivedy <swarupcp@gmail.com>
2025-11-01 15:44:12 +0530

    Update perfect-squares.cpp

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,5 +1,31 @@
 class Solution {
 public:
+    int solve(int n, vector<int>& dp){
+        if(n == 0){
+            return 0;
+        }
+
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        int minCount = INT_MAX;
+
+        for(int i = 1; i * i <= n; i++){
+            int result = 1 + solve(n - i * i, dp);
+            minCount = min(result, minCount);
+        }
+
+        return dp[n] = minCount;
+    }
+    int numSquares(int n) {
+        vector<int> dp(n + 1, -1);
+        return solve(n, dp);
+    }
+};
+
+class Solution {
+public:
     int numSquares(int n) {
         vector<int> dp(n + 1, INT_MAX);
         dp[0] = 0;
