3ed0bb324baa0a6cea3ac0ddfa6c5888845e0b00
Swarup Dwivedy <swarupcp@gmail.com>
2025-11-02 15:14:43 +0530

    Time: 80 ms (40.91%) | Memory: 88.2 MB (57.40%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
@@ -3,7 +3,7 @@ public:
     int n;
     int getNextIndex(int start, int currentJobEnd, vector<vector<int>>& array){
         int end = n - 1;
-        int result = n + 1;
+        int result = n;
 
         while(start <= end){
             int mid = start + (end - start) / 2;
@@ -17,27 +17,11 @@ public:
 
         return result;
     }
-    int solve(int i, vector<vector<int>>& array, vector<int>& dp){
-        if(i >= n){
-            return 0;
-        }
-
-        if(dp[i] != -1){
-            return dp[i];
-        }
-
-        int next = getNextIndex(i + 1, array[i][1], array);
-
-        int take = array[i][2] + solve(next, array, dp);
-        int notTake = solve(i + 1, array, dp);
-
-        return dp[i] = max(take, notTake);
-    }
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
         n = startTime.size();
 
         vector<vector<int>> array(n, vector<int>(3, 0));
-        vector<int> dp(n + 1, -1);
+        vector<int> dp(n + 1, 0);
 
         for(int i = 0; i < n; i++){
             array[i][0] = startTime[i];
@@ -47,6 +31,15 @@ public:
 
         sort(array.begin(), array.end());
 
-        return solve(0, array, dp);
+        for(int index = n - 1; index >= 0; index--){
+            int next = getNextIndex(index + 1, array[index][1], array);
+
+            int take = array[index][2] + dp[next];
+            int notTake = dp[index + 1];
+
+            dp[index] = max(take, notTake);
+        }
+
+        return dp[0];
     }
 };
