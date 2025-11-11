2d259aaf59380d759e550db91a131697ab353d13
Swarup Dwivedy <swarupcp@gmail.com>
2025-11-11 15:37:02 +0530

    Time: 193 ms (25.63%) | Memory: 81.9 MB (24.83%) - LeetSync

--- 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.cpp
+++ 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
-    int n;
-    int solve(string &s, int cur_index, int prev_val, vector<vector<int>> &dp){
-        if(cur_index >= n){
-            return 0;
-        }
-
-        if(dp[cur_index][prev_val] != -1){
-            return dp[cur_index][prev_val];
-        }
-
-        int flip = INT_MAX;
-        int notFlip = INT_MAX;
-
-        if(s[cur_index] == '1'){
-            if(prev_val == 1){
-                notFlip = solve(s, cur_index + 1, 1, dp);
-            }else{
-                flip = 1 + solve(s, cur_index + 1, 0, dp);
-                notFlip = solve(s, cur_index + 1, 1, dp);
-            }
-        }else{
-            if(prev_val == 1){
-                flip = 1 + solve(s, cur_index + 1, 1, dp);
-            }else{
-                flip = 1 + solve(s, cur_index + 1, 1, dp);
-                notFlip = solve(s, cur_index + 1, 0, dp);
-            }
-        }
-
-        return dp[cur_index][prev_val] = min(flip, notFlip);
-    }
     int minFlipsMonoIncr(string s) {
-        n = s.size();
-        vector<vector<int>> dp(n + 1, vector<int>(2, -1));
-        return solve(s, 0, 0, dp);
+        int n = s.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+
+        for(int i = n - 1; i >= 0; i--){
+            for(int prev_val = 0; prev_val <= 1; prev_val++){
+                int flip = INT_MAX;
+                int notFlip = INT_MAX;
+
+                if(s[i] == '1'){
+                    if(prev_val == 1){
+                        notFlip = dp[i + 1][1];
+                    }else{
+                        flip = 1 + dp[i + 1][0];
+                        notFlip = dp[i + 1][1];
+                    }
+                }else{
+                    if(prev_val == 1){
+                        flip = 1 + dp[i + 1][1];
+                    }else{
+                        flip = 1 + dp[i + 1][1];
+                        notFlip = dp[i + 1][0];
+                    }
+                }
+                dp[i][prev_val] = min(flip, notFlip);
+            }
+        }
+
+        return dp[0][0];
     }
 };
