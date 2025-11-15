97ad4f6a8c7e83136935eb131f62ccda1b842aaa
Swarup Dwivedy <swarupcp@gmail.com>
2025-11-15 14:12:48 +0530

    Time: 26 ms (21.86%) | Memory: 25 MB (27.76%) - LeetSync

---
+++ 1503-reducing-dishes/reducing-dishes.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int n;
+    int solve(int i, int time, vector<int>& satisfaction, vector<vector<int>>& dp){
+        if(i >= n){
+            return 0;
+        }
+
+        if(dp[i][time] != - 1){
+            return dp[i][time];
+        }
+
+        int include = satisfaction[i] * time + solve(i + 1, time + 1, satisfaction, dp);
+        int exclude = solve(i + 1, time, satisfaction, dp);
+
+        return dp[i][time] = max(include, exclude);
+    }
+    int maxSatisfaction(vector<int>& satisfaction) {
+        n = satisfaction.size();
+        sort(satisfaction.begin(), satisfaction.end());
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        return solve(0, 1, satisfaction, dp);
+    }
+};
