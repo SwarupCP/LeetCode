78fcb7bc55cfcbb821561fbf7b6d0eb110fb584e
Swarup Dwivedy <swarupcp@gmail.com>
2025-11-16 15:37:37 +0530

    Time: 23 ms (50.05%) | Memory: 13.1 MB (86.69%) - LeetSync

--- 87-scramble-string/scramble-string.cpp
+++ 87-scramble-string/scramble-string.cpp
@@ -1,41 +1,42 @@
 class Solution {
 public:
-    unordered_map<string, bool> mp;
-    bool solve(string s1, string s2){
-        if(s1 == s2){
-            return true;
-        }
-
-        if(s1.length() != s2.length()){
-            return false;
-        }
-
-        string key = s1 + "_" + s2;
-
-        if(mp.find(key) != mp.end()){
-            return mp[key];
-        }
-
-        bool result = false;
-        int n = s1.length();
-
-        for(int i = 1; i < n; i++){
-            bool swapped = solve(s1.substr(0, i), s2.substr(n - i, i)) && solve(s1.substr(i, n - i), s2.substr(0, n - i));
-            if(swapped == true){
-                result = true;
-                break;
-            }
-            bool not_swapped = solve(s1.substr(0, i), s2.substr(0, i)) && solve(s1.substr(i, n - i), s2.substr(i, n - i));
-            if(not_swapped == true){
-                result = true;
-                break;
-            }
-        }
-
-        return mp[key] = result;
-    }
     bool isScramble(string s1, string s2) {
-        mp.clear();
-        return solve(s1, s2);
+        int n = s1.size();
+        if (n != s2.size()) return false;
+        if (s1 == s2) return true;
+
+        vector<vector<vector<bool>>> dp(n, vector<vector<bool>>(n, vector<bool>(n + 1, false)));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j][1] = (s1[i] == s2[j]);
+            }
+        }
+
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                for (int j = 0; j <= n - len; j++) {
+
+                    bool possible = false;
+
+                    for (int k = 1; k < len; k++) {
+
+                        if (dp[i][j][k] && dp[i + k][j + k][len - k]) {
+                            possible = true;
+                            break;
+                        }
+
+                        if (dp[i][j + len - k][k] && dp[i + k][j][len - k]) {
+                            possible = true;
+                            break;
+                        }
+                    }
+
+                    dp[i][j][len] = possible;
+                }
+            }
+        }
+
+        return dp[0][0][n];
     }
 };
