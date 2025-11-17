4f6ef2844cf7bfc87829450578f0730cecbf6024
Swarup Dwivedy <swarupcp@gmail.com>
2025-11-17 15:25:49 +0530

    Time: 104 ms (89.08%) | Memory: 34.2 MB (67.58%) - LeetSync

--- 1393-maximum-value-of-k-coins-from-piles/maximum-value-of-k-coins-from-piles.cpp
+++ 1393-maximum-value-of-k-coins-from-piles/maximum-value-of-k-coins-from-piles.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxValueOfCoins(vector<vector<int>>& piles, int k) {
         int n = piles.size();
-        vector<vector<int>> dp(n + 1, vector<int>(k + 2, 0));
+        vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
 
         for(int i = n - 1; i >= 0; i--){
             for(int j = k; j >= 1; j--){
