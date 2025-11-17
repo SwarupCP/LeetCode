8e36677ee4ebc8ec2d5381ba63d9991c73a20269
Swarup Dwivedy <swarupcp@gmail.com>
2025-11-17 15:25:10 +0530

    Time: 103 ms (90.27%) | Memory: 34.1 MB (67.58%) - LeetSync

--- 1393-maximum-value-of-k-coins-from-piles/maximum-value-of-k-coins-from-piles.cpp
+++ 1393-maximum-value-of-k-coins-from-piles/maximum-value-of-k-coins-from-piles.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
-    int n;
-    int solve(int i, int k, vector<vector<int>>& piles, vector<vector<int>>& dp){
-        if(i >= n){
-            return 0;
-        }
-
-        if(dp[i][k] != -1){
-            return dp[i][k];
-        }
-
-        int notTaken = solve(i + 1, k, piles, dp);
-        int taken = 0;
-        int sum = 0;
-
-        for(int j = 0; j < min((int)piles[i].size(), k); j++){
-            sum += piles[i][j];
-
-            taken = max(taken, sum + solve(i + 1, k - (j + 1), piles, dp));
-        }
-
-        return dp[i][k] = max(taken, notTaken);
-    }
     int maxValueOfCoins(vector<vector<int>>& piles, int k) {
-        n = piles.size();
-        vector<vector<int>> dp(n + 1, vector<int>(k + 1, -1));
-        return solve(0, k, piles, dp);
+        int n = piles.size();
+        vector<vector<int>> dp(n + 1, vector<int>(k + 2, 0));
+
+        for(int i = n - 1; i >= 0; i--){
+            for(int j = k; j >= 1; j--){
+                int notTaken = dp[i + 1][j];
+                int taken = 0;
+                int sum = 0;
+
+                for(int k = 0; k < min((int)piles[i].size(), j); k++){
+                    sum += piles[i][k];
+
+                    taken = max(taken, sum + dp[i + 1][j - (k + 1)]);
+                }
+
+                dp[i][j] = max(taken, notTaken);
+            }
+        }
+        return dp[0][k];
     }
 };
