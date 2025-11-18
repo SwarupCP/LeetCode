d5e432c0916b00c4ace50cdcce4f232bdab5fec8
Swarup Dwivedy <swarupcp@gmail.com>
2025-11-18 15:47:49 +0530

    Time: 73 ms (72.32%) | Memory: 59.5 MB (42.82%) - LeetSync

--- 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -1,31 +1,12 @@
 class Solution {
 public:
-    int m, k;
     const int MOD = 1e9 + 7;
-    int solve(int i, int j, string &target, vector<vector<long long>>& freq, vector<vector<int>>& dp){
-        if(i == m){
-            return 1;
-        }
-
-        if(j == k){
-            return 0;
-        }
-
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-
-        int notTaken = solve(i, j + 1, target, freq, dp) % MOD;
-        int taken = (freq[target[i] - 'a'][j] * solve(i + 1, j + 1, target, freq, dp)) % MOD;
-
-        return dp[i][j] = (notTaken + taken) % MOD;
-    }
     int numWays(vector<string>& words, string target) {
-        m = target.length();
-        k = words[0].length();
+        int m = target.length();
+        int k = words[0].length();
 
         vector<vector<long long>> freq(26, vector<long long>(k));
-        vector<vector<int>> dp(m + 1, vector<int>(k + 1, -1));
+        vector<vector<int>> dp(m + 1, vector<int>(k + 1, 0));
 
         for(int col = 0; col < k; col++){
             for(string &word : words){
@@ -34,6 +15,19 @@ public:
             }
         }
 
-        return solve(0, 0, target, freq, dp);
+        for (int j = 0; j <= k; j++) {
+            dp[m][j] = 1;
+        }
+
+        for(int i = m - 1; i >= 0; i--){
+            for(int j = k - 1; j >= 0; j--){
+                int notTaken = dp[i][j + 1] % MOD;
+                int taken = (freq[target[i] - 'a'][j] * dp[i + 1][j + 1]) % MOD;
+
+                dp[i][j] = (notTaken + taken) % MOD;
+            }
+        }
+
+        return dp[0][0];
     }
 };
