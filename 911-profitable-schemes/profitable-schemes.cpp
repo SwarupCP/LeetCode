f72c2eb1ac886d67f1956d68b848c9e5901dabd9
Swarup Dwivedy <swarupcp@gmail.com>
2025-11-19 15:46:19 +0530

    Time: 271 ms (10.08%) | Memory: 289.6 MB (5.04%) - LeetSync

--- 911-profitable-schemes/profitable-schemes.cpp
+++ 911-profitable-schemes/profitable-schemes.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-    int N;
     const int MOD = 1e9 + 7;
-    int solve(int i, int p, int people, int &minProfit, vector<int>& group, vector<int>& profit, vector<vector<vector<int>>>& dp){
-        if(people > N){
-            return 0;
-        }
+    int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
+        int N = n;
+        int m = group.size();
+        vector<vector<vector<int>>> dp(101, vector<vector<int>>(101, vector<int>(101, 0)));
 
-        if(i == group.size()){
-            if(p >= minProfit){
-                return 1;
-            }else{
-                return 0;
+        for(int p = 0; p <= minProfit; p++){
+            for(int people = 0; people <= N; people++){
+                if(p >= minProfit){
+                    dp[m][p][people] = 1;
+                }else{
+                    dp[m][p][people] = 0;
+                }
             }
         }
 
-        if(dp[i][p][people] != -1){
-            return dp[i][p][people];
+        for(int i = group.size() - 1; i >= 0; i--){
+            for(int p = minProfit; p >= 0; p--){
+                for(int people = N; people >= 0; people--){
+                    int notTake = dp[i + 1][p][people] % MOD;
+                    int take = 0;
+                    if(people + group[i] <= N){
+                        take = dp[i + 1][min(p + profit[i], minProfit)][people + group[i]] % MOD;
+                    }
+
+                    dp[i][p][people] = (notTake + take) % MOD;
+                }
+            }
         }
 
-        int notTake = solve(i + 1, p, people, minProfit, group, profit, dp) % MOD;
-        int take = solve(i + 1, min(p + profit[i], minProfit), people + group[i], minProfit, group, profit, dp) % MOD;
-
-        return dp[i][p][people] = (notTake + take) % MOD;
-    }
-    int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
-        N = n;
-        vector<vector<vector<int>>> dp(101, vector<vector<int>>(101, vector<int>(101, -1)));
-        return solve(0, 0, 0, minProfit, group, profit, dp);
+        return dp[0][0][0];
     }
 };
