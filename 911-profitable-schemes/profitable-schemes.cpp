1b182d271482ff37d6627c0d65bdac0e9099757c
Swarup Dwivedy <swarupcp@gmail.com>
2025-11-19 15:48:41 +0530

    Time: 106 ms (40.32%) | Memory: 56.1 MB (36.34%) - LeetSync

--- 911-profitable-schemes/profitable-schemes.cpp
+++ 911-profitable-schemes/profitable-schemes.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     const int MOD = 1e9 + 7;
     int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
-        int N = n;
         int m = group.size();
-        vector<vector<vector<int>>> dp(101, vector<vector<int>>(101, vector<int>(101, 0)));
+        vector<vector<vector<int>>> dp(m + 1, vector<vector<int>>(minProfit + 1, vector<int>(n + 1, 0)));
 
         for(int p = 0; p <= minProfit; p++){
-            for(int people = 0; people <= N; people++){
+            for(int people = 0; people <= n; people++){
                 if(p >= minProfit){
                     dp[m][p][people] = 1;
                 }else{
@@ -18,10 +17,10 @@ public:
 
         for(int i = group.size() - 1; i >= 0; i--){
             for(int p = minProfit; p >= 0; p--){
-                for(int people = N; people >= 0; people--){
+                for(int people = n; people >= 0; people--){
                     int notTake = dp[i + 1][p][people] % MOD;
                     int take = 0;
-                    if(people + group[i] <= N){
+                    if(people + group[i] <= n){
                         take = dp[i + 1][min(p + profit[i], minProfit)][people + group[i]] % MOD;
                     }
 
