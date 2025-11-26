4dc53921a3dd43239da9017b9b3a86601648df8d
Swarup Dwivedy <swarupcp@gmail.com>
2025-11-26 15:34:58 +0530

    Time: 261 ms (60.19%) | Memory: 131.9 MB (83.17%) - LeetSync

--- 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.cpp
+++ 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
-    int m, n;
     const int MOD = 1e9 + 7;
-    int solve(int i, int j, int curSum, int k, vector<vector<int>>& grid, vector<vector<vector<int>>>& dp){
-        if(i >= m || j >= n){
-            return 0;
-        }
-
-        if(i == m - 1 && j == n - 1){
-            return (curSum + grid[i][j]) % k == 0 ? 1 : 0;
-        }
-
-        if(dp[i][j][curSum] != -1){
-            return dp[i][j][curSum];
-        }
-
-        int down = solve(i + 1, j, (curSum + grid[i][j]) % k, k, grid, dp) % MOD;
-        int right = solve(i, j + 1, (curSum + grid[i][j]) % k, k, grid, dp) % MOD;
-
-        return dp[i][j][curSum] = (down + right) % MOD;
-    }
     int numberOfPaths(vector<vector<int>>& grid, int k) {
-        m = grid.size();
-        n = grid[0].size();
-        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(k + 1, -1)));
-        return solve(0, 0, 0, k, grid, dp);
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(k, 0)));
+
+        for(int curSum = 0; curSum < k; curSum++){
+            if(((curSum + grid[m - 1][n - 1]) % k) == 0){
+                dp[m - 1][n - 1][curSum] = 1;
+            }
+        }
+
+        for(int i = m - 1; i >= 0; i--){
+            for(int j = n - 1; j >= 0; j--){
+                if(i == m - 1 && j == n - 1) continue; 
+                for(int curSum = 0; curSum < k; curSum++){
+                    int down = 0;
+                    if(i + 1 < m){
+                        down = dp[i + 1][j][(curSum + grid[i][j]) % k] % MOD;
+                    }
+                    int right = 0;
+                    if(j + 1 < n){
+                        right = dp[i][j + 1][(curSum + grid[i][j]) % k] % MOD;
+                    }
+
+                    dp[i][j][curSum] = (down + right) % MOD;
+                }
+            }
+        }
+        
+        return dp[0][0][0];
     }
 };
