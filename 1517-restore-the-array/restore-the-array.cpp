67b3e54deb699bcaa140f146ef444edcfd54f8a8
Swarup Dwivedy <swarupcp@gmail.com>
2025-11-28 14:40:08 +0530

    Time: 19 ms (89.18%) | Memory: 15.3 MB (91.03%) - LeetSync

--- 1517-restore-the-array/restore-the-array.cpp
+++ 1517-restore-the-array/restore-the-array.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
-    int n;
     const int MOD = 1e9 + 7;
-    int solve(int start, string &s, int k, vector<int>& dp){
-        if(start >= n){
-            return 1;
-        }
+    int numberOfArrays(string s, int k) {
+        int n = s.length();
+        vector<int> dp(n + 1, 0);
+        dp[n] = 1;
 
-        if(s[start] == '0'){
-            return 0;
-        }
+        for(int start = n - 1; start >= 0; start--){
 
-        if(dp[start] != -1){
-            return dp[start];
-        }
-
-        long ans = 0;
-        long long num = 0;
-
-        for(int end = start; end < n; end++){
-            num = (num * 10) + (s[end] - '0');
-
-            if(num > k){
-                break;
+            if(s[start] == '0'){
+                dp[start] = 0;
+                continue;
             }
 
-            ans = (ans % MOD + solve(end + 1, s, k, dp) % MOD) % MOD;
+            long ans = 0;
+            long long num = 0;
+
+            for(int end = start; end < n; end++){
+                num = (num * 10) + (s[end] - '0');
+
+                if(num > k){
+                    break;
+                }
+
+                ans = (ans % MOD + dp[end + 1] % MOD) % MOD;
+            }
+
+            dp[start] = ans;
         }
 
-        return dp[start] = ans;
-    }
-    int numberOfArrays(string s, int k) {
-        n = s.length();
-        vector<int> dp(n + 1, -1);
-        return solve(0, s, k, dp);
+        return dp[0];
     }
 };
